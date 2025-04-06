3a0ee539466bd61825ae97cfd2252fef49de872b
Lucas Clark <lucas.clark@berkeley.edu>
2025-04-06 15:12:58 -0700

    implement least squares estimator

--- src/bpm/mod.rs
+++ src/bpm/mod.rs
@@ -22,3 +22,43 @@ pub fn direct_count(timestamps: &[Instant]) -> Result<f64, BpmCalculationError> 
 
     Ok(bpm)
 }
+
+pub fn simple_regression(timestamps: &[Instant]) -> Result<f64, BpmCalculationError> {
+    if timestamps.len() < 2 {
+        return Err(BpmCalculationError::InsufficientData);
+    }
+
+    let start = timestamps.first().expect("timestamps should not be empty");
+    let n = timestamps.len() as f64;
+
+    // Slope of least squares regression line is equal to Cov(x, y) / Var(x)
+    // https://seismo.berkeley.edu/~kirchner/eps_120/Toolkits/Toolkit_10.pdf
+
+    let mean_x = timestamps
+        .iter()
+        .map(|ts| ts.duration_since(*start).as_millis())
+        .sum::<u128>() as f64
+        / n;
+    let mean_y = (n - 1_f64) / 2_f64;
+    let sum_x_squared = timestamps
+        .iter()
+        .map(|ts| ts.duration_since(*start).as_millis())
+        .map(|ms| ms * ms)
+        .sum::<u128>() as f64;
+    let sum_xy = timestamps
+        .iter()
+        .map(|ts| ts.duration_since(*start).as_millis())
+        .enumerate()
+        .map(|(i, ms)| i as u128 * ms)
+        .sum::<u128>() as f64;
+
+    // beats per millisecond
+    let slope = (sum_xy - n * mean_x * mean_y) / (sum_x_squared - n * mean_x * mean_x);
+
+    Ok(slope * 60_000_f64)
+}
+
+pub fn thiel_sen(timestamps: &[Instant]) -> Result<f64, BpmCalculationError> {
+    // The median of the slopes between every pair of points
+    todo!("Not Implemented");
+}
--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,20 @@ fn is_tap_key(key_code: &u32) -> bool {
     !disabled_keys.contains(key_code)
 }
 
+fn display_bpm_signal(bpm: ReadSignal<Option<f64>>) -> String {
+    match bpm.get() {
+        Some(bpm) => format!("{bpm:.2}"),
+        None => "Not Enough Data".into(),
+    }
+}
+
 #[component]
 fn App() -> impl IntoView {
     const RESET_SECS: u64 = 2;
 
     let (timestamps, set_timestamps) = signal::<Vec<Instant>>(Vec::new());
-    let (bpm_avg, set_bpm_avg) = signal::<Option<f64>>(None);
+    let (direct_count, set_direct_count) = signal::<Option<f64>>(None);
+    let (simple_regression, set_simple_regression) = signal::<Option<f64>>(None);
 
     let mut last_tap = Instant::now();
 
@@ -45,11 +53,16 @@ fn App() -> impl IntoView {
             } else {
                 set_timestamps.write().push(now);
             }
-            set_bpm_avg.set(
+            set_direct_count.set(
                 bpm::direct_count(&timestamps.read())
                     .inspect_err(|e| log!("{e:?}"))
                     .ok(),
             );
+            set_simple_regression.set(
+                bpm::simple_regression(&timestamps.read())
+                    .inspect_err(|e| log!("{e:?}"))
+                    .ok(),
+            )
         }
         last_tap = now;
     });
@@ -57,7 +70,8 @@ fn App() -> impl IntoView {
     view! {
         <main class="m-auto max-w-3xl text-center font-mono text-3xl">
             <p>"Total Beats: "{move || timestamps.get().len()}</p>
-            <p>"Average BPM: "{move || bpm_avg.get()}</p>
+            <p>"Direct Count Average: "{move || display_bpm_signal(direct_count)}</p>
+            <p>"Least Squares Estimate: "{move || display_bpm_signal(simple_regression)}</p>
         </main>
     }
 }
