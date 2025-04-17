312e2d78c69f189678a099c090446b298ecff439
Lucas Clark <lucas.clark@berkeley.edu>
2025-04-17 02:09:00 -0700

    finish interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use leptos::ev::{keydown, KeyboardEvent};
 use leptos::logging::log;
 use leptos::prelude::*;
+use leptos::wasm_bindgen::JsCast;
+use leptos::web_sys::Element;
 use leptos_use::{use_document, use_event_listener};
 use web_time::{Duration, Instant};
 
@@ -30,6 +32,10 @@ impl TapData {
                 self.timestamps = vec![0];
             }
         }
+    }
+    /// Returns true if the bpm count has been reset
+    pub fn is_reset(&self) -> bool {
+        self.start.is_none() && !self.timestamps.is_empty()
     }
 }
 
@@ -75,15 +81,35 @@ fn App() -> impl IntoView {
     });
 
     view! {
-        <div class="flex flex-col h-screen select-none" on:click={move |_| handle_beat_input()}>
-            <div class="flex justify-center items-center min-h-screen">
-                <pre class="font-mono bg-zinc-800 select-text text-white p-5" on:click={move |e| {
-                    e.prevent_default();
-                    e.stop_propagation();
-                }}>
-                    <Header />
+        <div class="flex flex-col h-screen" on:click={move |_| handle_beat_input()}>
+            <div class="flex justify-center items-center min-h-screen bg-zinc-800">
+                <pre
+                    // change border color if reset has happened
+                    class={move || {
+                        let pre_class = "font-mono bg-zinc-800 border-2 select-text text-white p-5";
+                        if tap_data.read().is_reset() {
+                            format!("{pre_class} border-orange-400")
+                        } else {
+                            format!("{pre_class} border-white")
+                        }
+                    }}
+                    on:click={move |e| {
+                        // prevent from triggering bpm click
+                        e.stop_propagation();
+                        // if target is not a button or link, prevent default (selecting text)
+                        if let Some(target) = e.target() {
+                            if let Some(el) = target.dyn_ref::<Element>() {
+                                let tag_name = el.tag_name().to_lowercase();
+                                if tag_name != "a" && tag_name != "button" {
+                                    e.prevent_default();
+                                }
+                            }
+                        }
+                    }}
+                >
+                    <span>"lucdar's bpm counter""\n\n"</span>
                     <BpmTable tap_data />
-                    <Footer />
+                    <Footer tap_data />
                 </pre>
             </div>
         </div>
@@ -92,51 +118,60 @@ fn App() -> impl IntoView {
 
 #[component]
 fn BpmTable(tap_data: ReadSignal<TapData>) -> impl IntoView {
-    // creates a <p> with the bpm calculated by the suppled function, labeled with the supplied label
+    // creates a row with formatted calculations
     macro_rules! render_bpm_metric {
-        ($label:expr, $algorithm:expr) => {
+        ($label:expr, $algorithm:expr, $description:expr) => {
             view! {
-                <span class="text-green-400">{format!("{:>12}: ", $label)}</span>
+                // align and color the label
+                <span class="text-green-400">
+                    {format!("{:>12}: ", $label)}
+                </span>
+                // conditionally color the measurement
                 <span class="text-violet-400">
                     {move || {
                         match $algorithm(&tap_data.read().timestamps)
                             .inspect_err(|e| log!("{e:?}"))
                             .ok()
                         {
-                            Some(bpm) => format!("{bpm:6.2}\n"),
-                            None => "000.00\n".into(),
+                            Some(bpm) => format!("{bpm:6.2} "),
+                            None => "000.00 ".into(),
                         }
                     }}
                 </span>
-                <span class="text-zinc-400"></span>
+                <span class="text-zinc-400">"# "{$description}"\n"</span>
             }
         };
     }
 
+    fn slice_len(ts: &[u64]) -> Result<u64, bpm::BpmCalculationError> {
+        Ok(ts.len() as u64)
+    }
+
     view! {
-        <span>
-            {format!("{:>12}: ", "n")}
-            {move || format!("{:6.2}\n", tap_data.read().timestamps.len())}
-        </span>
-        {render_bpm_metric!("direct", bpm::direct_count)}
-        {render_bpm_metric!("lin-reg", bpm::simple_regression)}
-        {render_bpm_metric!("thiel-sen", bpm::thiel_sen)}
+        {render_bpm_metric!("n", slice_len, "the total count of beats")}
+        {render_bpm_metric!("direct", bpm::direct_count, "n - 1 divided by delta t")}
+        {render_bpm_metric!("lin-reg", bpm::simple_regression, "simple linear regression")}
+        {render_bpm_metric!("thiel-sen", bpm::thiel_sen, "the \"median\" of the bpms")}
     }
 }
 
 #[component]
-fn Header() -> impl IntoView {
-    // TODO: make this
-    view! {<span>"lucdar's bpm counter\n\n"</span>}
-}
-
-#[component]
-fn Footer() -> impl IntoView {
-    // TODO: make this
+fn Footer(tap_data: ReadSignal<TapData>) -> impl IntoView {
+    let link_class = "hover:text-violet-400 transition-all duration-150";
     view! {<span>
-        "\n                                     "
-        "blog"
+        "\n"
+        <span class="text-orange-400">
+            {move ||
+                if tap_data.read().is_reset() {
+                    "reset!"
+                } else {
+                    "      "
+                }
+            }
+        </span>
+        {" ".repeat(31)}
+        <a href="https://laclark.me/blog/bpm/" class={link_class}>blog</a>
         " | "
-        "source"
+        <a href="https://github.com/lucdar/bpm-leptos/" class={link_class}>source</a>
     </span>}
 }
